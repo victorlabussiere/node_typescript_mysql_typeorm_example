a58faf210d47ce261f11844853acb7f5a463951f
Victor Labussiere <victor.labussiere95@gmail.com>
2024-02-25 19:55:43 -0300

    cors implementation

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,15 @@
 import "reflect-metadata"
 import 'dotenv'
+import cors, { CorsOptions } from 'cors'
 import express, { json } from 'express';
 import UserRoutes from './routes/UsersRoutes.js';
 import { MySQLDataSource } from "./config/MySQLConnection.js";
+
+let usersCorsOptions: CorsOptions = {
+    origin: 'http://localhost:3000',
+    methods: ['GET', 'POST'],
+    allowedHeaders: ['Content-Type']
+}
 
 const app = express();
 app.use(json())
@@ -11,9 +18,9 @@ MySQLDataSource.initialize()
     .then(() => console.log("Database is connected"))
     .catch(err => console.trace(err))
 
-app.use('/users', UserRoutes)
+app.use('/users', cors(usersCorsOptions), UserRoutes)
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3;
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
